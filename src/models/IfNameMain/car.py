6566a51805345345f89d8f128d719e8267b264c5
Tim Ruhkopf <tim.ruhkopf@googlemail.com>
2021-08-23 09:15:40 +0200

    moved ifnamemain tutorial in separate folder

---
+++ src/models/IfNameMain/car.py
@@ -0,0 +1,44 @@
+import numpy as np
+
+SOMECONST = 2
+
+def function(a, b, c=2):
+    """
+    Titel
+
+    kurzdefinition
+
+    was funktion tuen soll
+
+    :param a: is a value
+    :param b:
+    :param c:
+
+    :examples:
+
+    :return:
+    """
+    return a + b + c
+
+
+class Car:
+    def __init__(self):
+        """
+
+        """
+        np.random
+        pass
+
+    def accelerate(self):
+        """
+
+        :return:
+        """
+        pass
+
+
+if __name__ == '__main__':
+    SOMECONST2 = 3
+
+    somecar = Car()
+    somecar.accelerate()
---
+++ src/models/IfNameMain/main.py
@@ -0,0 +1,16 @@
+import src.models.IfNameMain.car
+
+from src.models.IfNameMain.car import SOMECONST
+
+CONSTANT = 1
+
+SOMECONST
+somecar = src.models.IfNameMain.car.Car()
+somecar.accelerate()
+
+# comment this
+
+# then do that
+car2 = car.Car()
+
+car.function(1, 2, 3)
